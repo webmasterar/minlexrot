c2cd643af97d5830e89840746399a2a7a8aba569
aretha <a_retha@ymail.com>
2025-07-08 01:55:38 +0100

    + min_lex_rot_count.py

--- README.md
+++ README.md
@@ -11,8 +11,8 @@ based on the _KMP_ search algorithm. It requires linear time and space. In 1983
 Duval devised an algorithm based on _Lyndon_ words that required linear time and
 constant space.
 
-Here you will find two new LMSR algorithms (KR and BW) -- one inspired by the
-_Karp-Rabin_ algorithm and the other based on _bitwise_ packing of a string into
+Here you will find three new LMSR algorithms (KR, BW and Count) -- KR is inspired
+by the _Karp-Rabin_ algorithm, BW is based on _bitwise_ packing of a string into
 a computer word (typically, a 64-bit int). The premise of both algorithms is to
 first pack each letter of a text into a computer word, then rotate it O(n) times,
 calculating a lexicographical score each time. The score is the numerical value
@@ -20,25 +20,28 @@ of the integer stored in the computer word and can be checked in constant time.
 Rotating the computer word can also be done in constant time. Thus, the algorithms
 require linear time and 8 bytes of extra space when the text fits into a single
 computer word, but in practice this means it is limited to short strings using
-a small constant alphabet.
+a small constant alphabet. Algorithm _Count_ is based on finding the longest
+contiguous stretch of lexicographically ascending characters in a string. It
+has no string length limits and runs in linear time.
 
-In this repository you will find the KR and BW single-word implementations
-written in Python:
+In this repository you will find the KR and BW single-word implementations, 
+and the Count algorithm written in Python:
 
  - min_lex_rot_kr.py
  - min_lex_rot_bw.py
+ - min_lex_rot_count.py
 
-Also in this repository, you will find a generalised implementation, which
-can handle longer strings by using more memory, but consequently _doesn't_ do
-it in linear time and uses up more space. Please refer to the comments of the
+Also in this repository, you will find a generalised implementation of KR and
+BW, which can handle longer strings by using more memory, but consequently _doesn't_
+do it in linear time and uses up more space. Please refer to the comments of the
 Python implementation for an analysis of the time and space complexity of that
 algorithm. There is also an implementation in C for greater performance:
 
  - generalised_min_lex_rot.py
  - generalised_min_lex_rot.c
 
-For long strings, I recommend using Booth's or Duval's algorithm, but for short
-ones, try the algorithms provided here.
+For long strings, I recommend using the Count algorithm presented here or use
+Booth's or Duval's algorithm. But for short string, try algorithms KR or BW.
 
 The code in this repository is provided only as a proof of concept with no
 guarantees. Please refer to the license.
---
+++ min_lex_rot_count.py
@@ -0,0 +1,143 @@
+# Copyright (c) 2025 Ahmad Retha; MIT License.
+#
+# This is an algorithm I am calling min_lex_rot_count. It solves the Lexicographically
+# Minimal String Rotation (LMSR) problem in O(n) time and O(a) extra space, where
+# n is the length of a text T and a is the size of a fixed-size alphabet A. It
+# works by finding the longest contiguous stretch of characters that is lexicographically
+# ascending and confirms that it is lexicographically minimal in the whole string.
+#
+# This is an alternative to Booth's algorithm (see https://en.wikipedia.org/wiki/Lexicographically_minimal_string_rotation).
+# 
+# How it works:
+# 
+# The intuition is simply that if we listed all the rotations of T and sorted
+# them in ascending order -- well, the first string would begin with the longest
+# contiguous stretch of characters that is both minimal and lexicographically
+# ascending.
+#
+# So all we have to do is find the longest contiguous stretch in any rotation
+# of T. We can find this by effectively concatenating T with itself, creating a
+# string T' of length 2*n-1 (where n = |T|). We look at each character T'[i] from
+# left to right, checking it is lexicographically equal to or greater than the
+# previous character and incrementing the count of this character in a table C
+# of size a = |A|. If we find that character T'[i] < T'[i-1], we end the stretch
+# and check to confirm that it is minimal so far in the text, then we reset the
+# current counter, begin a new stretch and continue looking through the rest of
+# the string.
+#
+# Analysis of the algorithm:
+#
+# Before we scan the text we need to create a table C for the counts of the alphabet
+# in the current stretch as well as another table M for the minimal stretch. The
+# size of each table is a = |A| and require O(a) time to create. We keep tabs on
+# the starting and ending position of the stretch using two pairs of variables
+# and this requires constant space and time to update.
+#
+# We scan through a sequence of 2n-1 characters in a text of length n. This takes
+# time O(n). With every character we update its count in a table C and the position
+# of the stretch in constant time.
+# 
+# There are two worst case string combination scenarios - a lexicographically
+# descending sequence and one where at every alternating position (i+1) the character
+# is lexicographically smaller. We then have to compare table C against M and
+# if the counts are greater then copy it to M. Finally, we have to reset C.
+# These actions take time O(a).
+#
+# So, for every position in T or every other position in T, we could be doing
+# O(a) operations, so the worst-case time complexity is O(na); Since the
+# alphabet is constant in a fixed-size alphabet (a = O(1)), the time complexity
+# could be said to be linear, O(n).
+# 
+# The space complexity of the algorithm is O(a) since we require to store counts
+# in tables C and M.
+#
+
+import argparse
+
+
+def _get_letter_idx(c, A):
+    return A.index(c)
+
+
+def _check_counts_better(min_counts, curr_counts, a):
+    j = 0
+    while j < a:
+        if curr_counts[j] > min_counts[j]:
+            return True
+        elif curr_counts[j] < min_counts[j]:
+            return False
+        j += 1
+    return False
+
+
+def minlexrot_count(seq, A):
+    n = len(seq)
+    a = len(A)
+    start_pos = 0
+    end_pos = 0
+    min_start_pos = 0
+    min_end_pos = 0
+    min_counts = [0] * a  # M
+    curr_counts = [0] * a  # C
+
+    # first letter
+    ltr_idx = _get_letter_idx(seq[0], A)
+    curr_counts[ltr_idx] = 1
+    min_counts[ltr_idx] = 1
+
+    i = 1
+    while i < 2*n-1:
+        prev_ltr = ltr_idx
+        ltr_idx = _get_letter_idx(seq[i%n], A)
+
+        # if curr_letter is not the same/bigger than previous letter
+        if prev_ltr > ltr_idx:
+
+            # check last stretch is greater than current one and if so copy it
+            if _check_counts_better(min_counts, curr_counts, a):
+                j = 0
+                while j < a:
+                    min_counts[j] = curr_counts[j]
+                    j += 1
+                min_start_pos = start_pos
+                min_end_pos = end_pos
+
+            # reset counts
+            j = 0
+            while j < a:
+                curr_counts[j] = 0
+                j += 1
+            curr_counts[ltr_idx] = 1
+            start_pos = i
+
+        else:
+
+            # count letter
+            curr_counts[ltr_idx] += 1
+
+        end_pos = i
+
+        i += 1
+
+    # check last stretch is greater than current one and if so update the min positions
+    if _check_counts_better(min_counts, curr_counts, a):
+        min_start_pos = start_pos
+        min_end_pos = end_pos
+
+    return min_start_pos, min_end_pos
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='min_lex_rot_count algorithm for finding the Lexicographically Minimal String Rotation')
+    parser.add_argument('-a', '--alphabet', type=str, required=True, help='The alphabet, e.g. "ACGT"')
+    parser.add_argument('-t', '--text', type=str, required=True, help='The input string')
+    args = parser.parse_args()
+    
+    T = args.text
+    A = args.alphabet
+    n = len(T)
+    start_pos, end_pos = minlexrot_count(T, A)
+    # print(T)
+    # print(start_pos, end_pos)
+    TT = T + T
+    print(TT[start_pos:start_pos+n])
